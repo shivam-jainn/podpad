96c83587cb6fcf7eef895a738bf633f4565eee5b
shivam-jainn <shivam.clgstash@gmail.com>
2024-09-12 23:58:48 +0530

    feat(FileSystem): :sparkles: Introduced Tree based Sidebar , which is draggable
    Uses AntD tree component . This needs a lot of work to be polished

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Editor from "./editor/Editor";
+import Welcome from "./welcome/Welcome";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Welcome />,
+  },
+  {
+    path : "/editor",
+    element: <Editor/>
+  }
+]);
+
 
 function App() {
-  return <div>Welcome to podspace</div>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
